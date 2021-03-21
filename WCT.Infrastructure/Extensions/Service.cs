6a678cacb17397fe5f1c0df8beb6b88df5bb44a2
Sulejman Ferizovic <sulejman.ferizovic@gmail.com>
2021-03-21 12:05:55 +0100

    Add Swagger and Cors Configuration

---
+++ WCT.Infrastructure/Extensions/Service.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace WCT.Infrastructure.Extensions
+{
+    public static class Service
+    {
+        public static void ConfigureCors(this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+               {
+                   builder.AllowAnyHeader()
+                   .WithMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                   .AllowCredentials()
+                   .WithOrigins(configuration
+                      .GetSection("AllowedHosts")
+                      .Get<string[]>());
+               });
+            });
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "WCT API",
+                    Version = "v1"
+                });
+
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string []{}
+                    }
+                });
+            });
+        }
+    }
+}
